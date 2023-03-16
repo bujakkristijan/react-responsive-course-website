9a5435cbd7a8fc05d79a0da3e06a44ead0769f8f
Kristijan Bujak <cileb411@gmail.com>
2023-03-16 23:37:17 +0100

    responsive navbar sa hamburger menu za male ekrane

--- react-course-app/src/components/home/HomeComponent.js
+++ react-course-app/src/components/home/HomeComponent.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import './HomeComponent.css';
 
 const HomeComponent = () => {
   return (
     <div className='home-main-container'>
         <div className='home-content-container'>
+          <h1 className='title'>World's biggest university</h1>
+          <p className='paragraph'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <Link to='' className='visit-btn'>Visit us to know more</Link>
         </div>
         
     </div>
--- react-course-app/src/components/navbar/NavbarComponent.js
+++ react-course-app/src/components/navbar/NavbarComponent.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { Link } from 'react-router-dom'
 import './NavbarComponent.css';
 
 const NavbarComponent = () => {
+    const [counter, setCounter] = useState(0)
+    useEffect(() => {
+        console.log("render");
+    }, [counter])
+    
+
+    
+
+    const hideMenu = () =>{
+        const navLinks = document.getElementById("navLinks");
+        navLinks.style.right = "-200px";
+    }
+
+    const showMenu = () =>{
+        const navLinks = document.getElementById("navLinks");
+        navLinks.style.right = "0"; // po defaultu u css-u za male ekrane je stavljeno da je -200, ukoliko stisne dugme, postaje right: 0, pa se pojavi  meni
+    }
+
   return (
     <nav className='navbar'>
         <Link to='/home' className='nav-logo' >
             <img className='img-logo' src={require('../../images/logo.png')} alt=''></img>
         </Link>
-        <div className='nav-links'>
-            <ul>
+        <div className='nav-links' id='navLinks'>
+            <i className="fa fa-times" onClick={hideMenu}></i>
+            <ul className='nav-ul'>
                 <li className='nav-list-item'>
                     <Link className='nav-link'>Home</Link>
                 </li>
@@ -28,7 +47,7 @@ const NavbarComponent = () => {
                 
             </ul>
         </div>
-
+        <i id="menu" className="fa fa-bars" onClick={showMenu}></i>
     </nav>
   )
 }
